f9aa1f717fa7761428cf25b9969d89ca54f85b42
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-23 15:23:27 +0100

    solidifying knowledge

--- factorialNumberr.py
+++ factorialNumberr.py
@@ -2,7 +2,11 @@ def factorial(number):
     if number == 0:
         return 1
     else:
-    totalFactorial = 1
-    while number > 1:
-        totalFactorial = 
+        totalFactorial = 1
+        while number > 1:
+            totalFactorial = totalFactorial * number
+            number = number - 1
+        return totalFactorial
+
+print(factorial(4))
     
