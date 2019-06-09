cd0369d82140e9ea5445c705f34bb474587449bc
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-06-09 11:48:23 +0100

    solidifying knowledge

--- calc_factorial.py
+++ calc_factorial.py
@@ -11,25 +11,4 @@ def factorial(number):
 
 print (factorial(9))
 
-def checkFactorial(number):
-    if number == 0:
-        return 1
-    total = 1
-    while number > 1:
-        total = total * number
-        number -= 1
-    return total
-
-
-print(checkFactorial(5))
-
-def calculateFactorial(number):
-  if number == 0:
-    return 1
-  factorialFromNumber = 1
-  while number > 1:
-    factorialFromNumber = factorialFromNumber * number
-    number -= 1
-  return factorialFromNumber
-
 print(calculateFactorial(8))
